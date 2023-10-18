36c21795a965216ee6cb68585278fbda4eede28c
Andersonbenevak <andersonbenevak@gmail.com>
2023-10-17 21:36:44 -0300

    Criando testes da classe Acesso e adicinando os pacotes Controller,Service e Repository.

--- virtual/src/main/java/com/loja/virtual/VirtualApplication.java
+++ virtual/src/main/java/com/loja/virtual/VirtualApplication.java
@@ -2,8 +2,17 @@ package com.loja.virtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 
 @SpringBootApplication
+@EntityScan("com.loja.virtual.model")
+@ComponentScan(basePackages = {"com.*"})
+@EnableJpaRepositories(basePackages = {"com.loja.virtual.repository"})
+@EnableTransactionManagement
 public class VirtualApplication {
 
 	public static void main(String[] args) {
